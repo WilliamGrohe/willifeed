985887c15e8be8d54c36ec6b40dc8e5b22f88b6f
WilliamGrohe <william.grohe@gmail.com>
2023-11-10 02:44:55 -0300

    Created Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 
 import { Header } from './components/Header'
 
+import styles from './App.module.css'
+
+import './global.css'
+import { Sidebar } from './components/Sidebar'
+
 function App() {
 
   return (
     <>
     <Header />
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore esse voluptatum dignissimos! Sed id architecto accusantium unde laudantium, officiis, omnis tenetur in mollitia neque libero ratione nemo. Tempore, natus doloribus.</p>
+    
+    <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quas nesciunt quod natus tempora iusto iste doloribus, blanditiis, a debitis, autem animi deleniti eum laborum. Totam nam quos voluptatibus non!</p>
+
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur non at consectetur beatae maiores esse facilis. Eum, veritatis molestiae, dolore est eligendi quasi officia numquam aut hic reiciendis fuga harum!</p>
+      </main>
+    </div>
     </>
   )
 }
