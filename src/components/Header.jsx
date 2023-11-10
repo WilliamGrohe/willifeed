cc50f97efbdabf19e1856c5219eefa6898075f61
WilliamGrohe <william.grohe@gmail.com>
2023-11-10 01:49:15 -0300

    Created Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import styles from './Header.module.css'
 
-// import willifeedLogo from '../assets/willifeed-logo.svg'
+import willifeedLogo from '../assets/willifeed-logo.svg'
 
 export function Header() {
   return(
     <header className={styles.header}>
-      <img src={'willifeedLogo'} alt="" />
+      <img src={willifeedLogo} alt="Logotipo do WilliFeed" />
+      <h1>WilliFeed</h1>
     </header>
   )
 }
