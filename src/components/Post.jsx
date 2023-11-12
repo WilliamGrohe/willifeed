1cc7e87f00fac1f70e099dc51b875d391fe0c4c0
WilliamGrohe <william.grohe@gmail.com>
2023-11-12 01:17:52 -0300

    Add comment form to post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,34 +1,51 @@
-import styles from './Post.module.css'
+import styles from "./Post.module.css";
 
 export function Post() {
-  return(
+  return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="http://www.github.com/williamgrohe.png" />
+          <img
+            className={styles.avatar}
+            src="http://www.github.com/williamgrohe.png"
+          />
           <div className={styles.authorInfo}>
             <strong>William Grohe</strong>
             <span>Web Developer</span>
           </div>
         </div>
-        
-        <time title='12 de novembro às 00:08' dateTime='2023-11-12 00:08:32'>Publicado há 1 hora.</time>
+
+        <time title="12 de novembro às 00:08" dateTime="2023-11-12 00:08:32">
+          Publicado há 1 hora.
+        </time>
       </header>
 
       <div className={styles.content}>
-      <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p>👉 {' '}<a href="">jane.design/doctorcare</a></p>
+        <p>Fala galeraa 👋</p>
         <p>
-          <a href="">#novoprojeto</a>{' '}
-          <a href="">#nlw</a>{' '}
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉 <a href="">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
           <a href="">#rocketseat</a>
         </p>
       </div>
 
       <div className={styles.divider}></div>
 
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
 
+        <textarea placeholder="Deixe um comentário" />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
-  )
+  );
 }
