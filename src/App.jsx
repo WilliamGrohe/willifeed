bfb99487987b9153037e674c7401c07d7e5b3c8c
WilliamGrohe <william.grohe@gmail.com>
2023-11-12 00:49:40 -0300

    Create Post component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import styles from './App.module.css'
 
 import './global.css'
 import { Sidebar } from './components/Sidebar'
+import { Post } from './components/Post'
 
 function App() {
 
@@ -15,10 +16,8 @@ function App() {
     <div className={styles.wrapper}>
       <Sidebar />
       <main>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quas nesciunt quod natus tempora iusto iste doloribus, blanditiis, a debitis, autem animi deleniti eum laborum. Totam nam quos voluptatibus non!</p>
-
-
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur non at consectetur beatae maiores esse facilis. Eum, veritatis molestiae, dolore est eligendi quasi officia numquam aut hic reiciendis fuga harum!</p>
+        <Post />
+        <Post />
       </main>
     </div>
     </>
---
+++ src/components/Post.jsx
@@ -0,0 +1,34 @@
+import styles from './Post.module.css'
+
+export function Post() {
+  return(
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="http://www.github.com/williamgrohe.png" />
+          <div className={styles.authorInfo}>
+            <strong>William Grohe</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        
+        <time title='12 de novembro às 00:08' dateTime='2023-11-12 00:08:32'>Publicado há 1 hora.</time>
+      </header>
+
+      <div className={styles.content}>
+      <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉 {' '}<a href="">jane.design/doctorcare</a></p>
+        <p>
+          <a href="">#novoprojeto</a>{' '}
+          <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+
+      <div className={styles.divider}></div>
+
+
+    </article>
+  )
+}
