c4269c6821e1e594b77416138cfd29dc0f376ed5
WilliamGrohe <william.grohe@gmail.com>
2023-11-16 02:13:54 -0300

    Validated empty comment form

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,46 +1,50 @@
-import { useState } from 'react';
-import { format, formatDistanceToNow } from 'date-fns'
-import ptBR from 'date-fns/locale/pt-BR'
+import { useState } from "react";
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 
 import { Comment } from "./Comment";
-import { Avatar } from './Avatar'
+import { Avatar } from "./Avatar";
 import styles from "./Post.module.css";
-import { Cpu } from '@phosphor-icons/react';
+import { Cpu } from "@phosphor-icons/react";
 
-export function Post({author, content, publishedAt}) {
-  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
-    locale: ptBR,
-  })
+export function Post({ author, content, publishedAt }) {
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    {
+      locale: ptBR,
+    }
+  );
 
   const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
     locale: ptBR,
     addSuffix: true,
-  })
+  });
 
-  const [comments, setComments] = useState([
-    'Primeiro post!'
-  ])
+  const [comments, setComments] = useState(["Primeiro post!"]);
 
-  const [newCommentText, setNewCommentText] = useState('')
+  const [newCommentText, setNewCommentText] = useState("");
 
   function handleCreateNewComment() {
-    event.preventDefault()
+    event.preventDefault();
 
-    setComments([...comments, newCommentText])
-    setNewCommentText('')
+    setComments([...comments, newCommentText]);
+    setNewCommentText("");
   }
 
   function handleNewCommentChange() {
-    setNewCommentText(event.target.value)
+    setNewCommentText(event.target.value);
   }
 
   function deleteComment(commentToDelete) {
-    const commentsWithoutDeletedOne = comments.filter(comment => {
-      return comment !== commentToDelete
-    })
+    const commentsWithoutDeletedOne = comments.filter((comment) => {
+      return comment !== commentToDelete;
+    });
 
-    setComments(commentsWithoutDeletedOne)
+    setComments(commentsWithoutDeletedOne);
   }
+
+  const isNewCommentEmpty = newCommentText.length == 0;
 
   return (
     <article className={styles.post}>
@@ -53,18 +57,25 @@ export function Post({author, content, publishedAt}) {
           </div>
         </div>
 
-        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
-          {publishedDateRelativeToNow} 
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        {content.map(line => {
-            if ( line.type === 'paragraph'){
-              return <p key={content.line}>{line.content}</p>;
-            } else if (line.type === 'link') {
-              return <p key={content.line}><a href="#">{line.content}</a></p>
-            }
+        {content.map((line) => {
+          if (line.type === "paragraph") {
+            return <p key={content.line}>{line.content}</p>;
+          } else if (line.type === "link") {
+            return (
+              <p key={content.line}>
+                <a href="#">{line.content}</a>
+              </p>
+            );
+          }
         })}
       </div>
 
@@ -73,16 +84,29 @@ export function Post({author, content, publishedAt}) {
       <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
-        <textarea name="commentInput" onChange={handleNewCommentChange} value={newCommentText} placeholder="Deixe um comentário" />
+        <textarea
+          name="commentInput"
+          onChange={handleNewCommentChange}
+          value={newCommentText}
+          placeholder="Deixe um comentário"
+        />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
 
       <div className={styles.commentList}>
-        {comments.map(comment => {
-          return <Comment key={comment} content={comment} onDeleteComment={deleteComment}/>
+        {comments.map((comment) => {
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          );
         })}
       </div>
     </article>
